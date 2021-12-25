a489ec66f503fb193d7cb4d9e960d6f549e41a32
Md Salim Rana <rana.salim.info@gmail.com>
2021-12-25 05:14:18 +0000

    Learn GET method of Express

--- app.js
+++ app.js
@@ -3,5 +3,16 @@ const app = express()
 
 const port = 5000
 
+app.get('/:slug', (request, response) => {
+  const slug = request.params.slug
+  const name = request.query.name
+  const age = request.query.age
+  const habit = request.query.habit
+
+  response.json({
+    message: slug,
+    person: `${name} who is ${age} has a habit of ${habit}`
+  })
+})
+
 app.listen(port, () => console.log('Server running on port: ' + port))
-// test
